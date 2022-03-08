63ecdd3de66d2f6a180838cfb90ce2b13d6f96bb
Dania Tamim <dania.tamim@live.se>
2022-03-08 17:13:13 +0100

    bootstrap

---
+++ Lab-7/scripts.js
@@ -0,0 +1,11 @@
+const volymKub= function(bredd,längd,höjd){
+    let volymK= bredd*längd*höjd;
+    return volymK;
+}
+
+const volymKub2= function(side){
+    let volymK= Math.pow(side,3);
+    return volymK;
+}
+
+
