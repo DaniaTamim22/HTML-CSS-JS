9a5cc05322a5dea42a8c70086a3020248c1da3ce
Dania Tamim <dania.tamim@live.se>
2022-03-25 09:22:09 +0100

    lektion-8-9

---
+++ Lektion-8/Form_validation/scripts.js
@@ -0,0 +1,26 @@
+const form= document.querySelector('#regForm');
+const firstName= document.querySelector('#firstName')
+
+
+const validateText= (input)=> {
+if
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+form.addEventListener('submit',e => {
+    e.preventDefault();
+})
