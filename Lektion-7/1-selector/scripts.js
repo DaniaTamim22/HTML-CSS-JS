916dacd27578579cf37a07039c0a40d651de9280
Dania Tamim <dania.tamim@live.se>
2022-03-30 04:27:58 +0200

    lektion-7

---
+++ Lektion-7/1-selector/scripts.js
@@ -0,0 +1,7 @@
+let  p1= document.querySelector('p')
+// console.log(p1)
+
+let q2 =document.querySelector('.paragraf')
+// console.log(q2)
+let q3= document.querySelector('div.paragraf')
+console.log(q3)
---
+++ Lektion-7/2-events/scripts.js
@@ -0,0 +1,30 @@
+const btn= document.querySelector('#btnAlert')
+const square=document.querySelector('#square')
+const loginForm = document.querySelector('#loginForm')
+const firstName= document.querySelector('#firstName')
+
+
+btn.addEventListener('click', () => {
+console.log('eventet kördes')
+})
+
+
+
+square.addEventListener('mousemove',(e)=>{
+    // console.log(e.offsetX,e.offsetY)
+square.style.background=`rgb(${e.offsetX},${e.offsetY},0)`
+})
+
+
+
+firstName.addEventListener('keyup', e =>{
+    console.log(e.key)
+})
+
+firstName.addEventListener('focus',e=>{
+    firstName.classList.add('focus-red')
+})
+ 
+firstName.addEventListener('blur',e=>{
+    firstName.classList.remove('focus-red')
+})
