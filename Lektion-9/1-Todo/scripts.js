fe1002cf414ff9dbead9ab5e37939e4efc7e7428
Dania Tamim <dania.tamim@live.se>
2022-05-11 14:03:47 +0200

    Lektion-9

--- Lektion-9/1-Todo/scripts.js
+++ Lektion-9/1-Todo/scripts.js
@@ -25,16 +25,61 @@ const listTodos = () => {
 
 
 const createTodoElement = (todo, parent, placement, isNew) => {
+    let title = todo.title.replace('<', '&lt').replace('>', '&gt')
     parent.insertAdjacentHTML(placement, `
     <div id= "todo_${todo.id}" class="border-bottom py-2 ${isNew ? 'slide-in' : ''}">
             <div div class= " d-flex justify-content-between align-items-center" >
-        <p id="title_${todo.id}"class="h4 m-0 todo-title ${todo.completed ? 'complete' : ''}">${todo.title}</p>
+        <p id="title_${todo.id}"class="h4 m-0 todo-title ${todo.completed ? 'complete' : ''}">${title}</p>
     <button id="delete_${todo.id}"class="btn btn-danger btn-small" >X</button >
    </div>
     </div >
     `)
     addRemoveOnClick(todo)
+    addToggleComplete(todo)
 }
+
+
+const addToggleComplete = (todo) => {
+    document.querySelector(`#title_${todo.id}`).addEventListener('click', () => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                id: todo.id,
+                completed: !todo.completed,
+                title: todo.title
+
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+            .then(res => {
+                if (!res.ok) {
+                    error.innerText = 'Something went wrong'
+                    error.classList.remove('d-none')
+                    return
+                }
+                error.classList.add('d-none')
+                return res.json()
+            })
+            .then(data => {
+                //console.log(data)
+                const _todo = document.querySelector('#todo_' + data.id)
+                createTodoElement(data, _todo, 'beforebegin', false)
+                _todo.remove()
+            })
+            .catch(err => console.log(err))
+
+        // todo.completed = !todo.completed
+        // console.log(todos)
+        // const todoElement = document.querySelector('#todo_' + todo.id)
+        // createTodoElement(todo, todoElement, 'beforebegin', false)
+        // todoElement.remove()
+
+    })
+}
+
+
 
 
 const addRemoveOnClick = (todo) => {
@@ -42,6 +87,7 @@ const addRemoveOnClick = (todo) => {
         if (todo.completed) {
             error.classList.add('d-none')
             //delete todo
+            deleteTodo(todo)
         }
         else {
             error.innerText = 'You need to complete the todo first!'
